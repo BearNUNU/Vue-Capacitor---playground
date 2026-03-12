cb8c7bdf30466c5ced07708ddcdaef3b5a8d0fd1
BearNUNU <kkw31025@gmail.com>
2026-03-12 14:11:45 +0900

    feat: 쿠키 매니저와 httponly 쿠키 사용을 위한 capacitor 옵션 추가

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,20 @@ const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'vue-capacitor-playground',
   webDir: 'dist',
+  server: {
+    androidScheme: 'http',
+    cleartext: true, // http 통신이 기본적으로 차단이기에 이를 허용하는 설정
+  },
   plugins: {
     StatusBar: {
       visible: true,
       overlaysWebView: false
+    },
+    CapacitorCookies: { // HttpOnly 쿠키를 사용하기 위한 설정
+      enabled: true,
+    },
+    CapacitorHttp: { // Http 통신을 사용하기 위한 설정, fetch() & axios의 통신을 인터셉트한다.
+      enabled: true,
     }
   }
 };
